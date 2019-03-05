0170625843206c7002f151563c4ff660fbafcf75
Shubham Mishra <shubham.mishra@successive.tech>
2019-03-05 18:21:38 +0530

    feat

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,48 +1,27 @@
-// import React from 'react'
-// import './style.css'
-// const test = ()=>{
-
-//    return (
-//     <div>
-//         <h1>This is valid Input</h1>
-//         <input type="text" className="formControl" placeholder="Accessible" />
-//         <h1>This is Disabled Input</h1>
-//         <input type="text" className="formControl" placeholder="Disabled Input" disabled="disabled" />
-//         <h1>Input Box with errors</h1>
-//         <div className="error">
-//             <input type="text" className="errorbox formControl" placeholder="" /><br />
-//             Contains Error
-//         </div>
-        
-//     </div>
-       
-//    )
-// }
-// export default test;
 import React from 'react'
 import './style.css'
-const test = (props)=>{
+const test = (props) => {
     let input = null;
     const { value } = props;
-    switch( value ){
+    switch (value) {
         case 'Access':
             input = <input type="text" className="formControl" placeholder="Accessible" />;
-        break;
+            break;
         case 'Disabled':
             input = <input type="text" className="formControl" placeholder="Disabled Input" disabled="disabled" />;
-        break;
+            break;
         default:
             input = (
                 <div className="error">
-                    <input type="text" className="errorbox" placeholder="" />
+                    <input type="text" className="errorbox formControl" placeholder="ERROR" />
                     <p>Error field</p>
                 </div>
             )
     };
-   return (
-<div>
-    {input}
-</div>      
-   )
+    return (
+        <div>
+            {input}
+        </div>
+    )
 }
 export default test;
--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Textfield from '../../components/TextField/TextField'
 
 
-const test = ()=>{
-   return (
-       <div>
-       <Textfield value = 'Access' />
-       <Textfield value = 'Disabled' />
-       <Textfield value = '' />
-       </div> 
-   )
+const test = () => {
+    return (
+        <div>
+            <Textfield value='Access' />
+            <Textfield value='Disabled' />
+            <Textfield value='' />
+        </div>
+    )
 }
-export default test; 
+export default test;
