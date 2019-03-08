b25004a48b5f083c82fd2f1d4f35da63a292b48e
Shubham Mishra <shubham.mishra@successive.tech>
2019-03-08 12:28:52 +0530

    feat

--- src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -6,7 +6,7 @@ const TextField = () => {
     return (
         <div>
             This is Accessible Text Input
-            <Textfield value='' type="text"/>
+            <Textfield value='' type="text" />
             This is Disabled Text Input
             <Textfield value='Disabled' />
             Text Input with Error
